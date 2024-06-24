df7e84b716f8a1a6f239afe6a96dacb12a3a4b68
Minh Vo <quang.minh@snaptec.co>
2024-06-24 10:49:23 +0700

    fix: remove packages react-hooks-global-state and update the source code for useState of React only

--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -1,5 +1,6 @@
 import SwitchDarkMode from '@/components/switch-dark-mode'
 import Image from 'next/image'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
